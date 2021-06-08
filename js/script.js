482d3d8b9286ed6f3e978d6466c03f3a112f225f
johannranudd <33978097+johannranudd@users.noreply.github.com>
2021-06-08 10:43:06 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -4,13 +4,15 @@ const button = document.querySelector(".changeColorButton");
 let colors = ["green", "red", "#F15025", "Rgba(133,122,200)", "#b00b69"];
 
 
-
+// on load
 window.addEventListener("load", function() {
   let randomNumber = Math.floor(Math.random() * colors.length);
   body.style.backgroundColor = colors[randomNumber];
   display.innerHTML = `<h2>Background Color:</h2> <h2>${colors[randomNumber]}</h2>`;
 });
 
+
+// on click
 button.addEventListener("click", function() {
     let randomNumber = Math.floor(Math.random() * colors.length);
     body.style.backgroundColor = colors[randomNumber];
